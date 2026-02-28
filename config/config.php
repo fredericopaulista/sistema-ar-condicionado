7c4957d148c45fce2e609f777f28fb58dad02ab6
fredericopaulista <fredericopaulista@gmail.com>
2026-02-28 01:37:07 -0300

    Remove Assinafy and mailer configuration settings from `config.php`.

--- config/config.php
+++ config/config.php
@@ -8,16 +8,5 @@ return [
         'pass' => '?*4kqWSh1N0V;+~1',
         'charset' => 'utf8mb4'
     ],
-    'assinafy' => [
-        'api_key' => 'IclOq0miwtZ13F3Up8tzVQaZMVotCrj0lxIEt8Nq9ml2Q3FYnTSCxXgLV4GQd4uy',
-        'base_url' => 'https://api.assinafy.com.br/v1'
-    ],
-    'mail' => [
-        'host' => 'mail.soarcondicionadobh.com.br',
-        'port' => 465,
-        'user' => 'noreply@soarcondicionadobh.com.br',
-        'pass' => 'OTGX(kj_IL,Ai#R)',
-        'from_name' => 'SÓ AR BH - Climatização'
-    ],
     'app_url' => 'https://crm.soarcondicionadobh.com.br'
 ];
